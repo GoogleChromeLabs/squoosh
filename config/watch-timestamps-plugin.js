ccf71556a626cd6ca9f44f32bc8f2f8f3d0caaeb
Jason Miller <developit@google.com>
2018-03-27 20:39:13 -0400

    Use classes for webpack plugins.

--- config/watch-timestamps-plugin.js
+++ config/watch-timestamps-plugin.js
@@ -1,23 +1,23 @@
-let fs = require('fs');
+const fs = require('fs');
 
-module.exports = WatchTimestampsPlugin;
+module.exports = class WatchTimestampsPlugin {
+	constructor(patterns) {
+		this.patterns = patterns;
+	}
 
-function WatchTimestampsPlugin(patterns) {
-	this.patterns = patterns;
-}
+	apply(compiler) {
+		compiler.plugin('watch-run', (watch, callback) => {
+			let patterns = this.patterns;
+			let timestamps = watch.fileTimestamps || watch.compiler.fileTimestamps;
 
-WatchTimestampsPlugin.prototype.apply = function (compiler) {
-	compiler.plugin('watch-run', (watch, callback) => {
-		let patterns = this.patterns;
-		let timestamps = watch.fileTimestamps || watch.compiler.fileTimestamps;
-
-		Object.keys(timestamps).forEach( filepath => {
-			if (patterns.some( pat => pat instanceof RegExp ? pat.test(filepath) : filepath.indexOf(pat)===0 )) {
-				let time = fs.statSync(filepath).mtime;
-				if (timestamps instanceof Map) timestamps.set(filepath, time);
-				else timestamps[filepath] = time;
-			}
+			Object.keys(timestamps).forEach(filepath => {
+				if (patterns.some(pat => pat instanceof RegExp ? pat.test(filepath) : filepath.indexOf(pat) === 0)) {
+					let time = fs.statSync(filepath).mtime;
+					if (timestamps instanceof Map) timestamps.set(filepath, time);
+					else timestamps[filepath] = time;
+				}
+			});
+			callback();
 		});
-		callback();
-	});
+	}
 };
