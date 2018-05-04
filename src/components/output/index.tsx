444e59c69c2bdb1ddcf1e10a5deb7dc1f9e38615
Jake Archibald <jaffathecake@gmail.com>
2018-05-04 20:03:57 +0100

    Merging pinch-zoom (#27)
    * Merging pinch-zoom
    
    * Pixelated output

--- src/components/output/index.tsx
+++ src/components/output/index.tsx
@@ -1,6 +1,5 @@
 import { h, Component } from 'preact';
-// This isn't working.
-// https://github.com/GoogleChromeLabs/squoosh/issues/14
+import './custom-els/PinchZoom';
 import * as style from './style.scss';
 
 type Props = {
@@ -36,7 +35,9 @@ export default class App extends Component<Props, State> {
   render({ img }: Props, { }: State) {
     return (
       <div>
-        <canvas ref={c => this.canvas = c as HTMLCanvasElement} width={img.width} height={img.height} />
+        <pinch-zoom>
+          <canvas class={style.outputCanvas} ref={c => this.canvas = c as HTMLCanvasElement} width={img.width} height={img.height} />
+        </pinch-zoom>
         <p>And that's all the app does so far!</p>
       </div>
     );
