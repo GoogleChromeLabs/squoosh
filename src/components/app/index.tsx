b069c7f89aa6c7013fb48a64903633513f4c8e30
Jason Miller <jason@developit.ca>
2018-06-19 20:08:52 +0000

    Refactor state management to use an Array of objects and immutable updates instead of relying on explicit update notifications.

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -20,27 +20,32 @@ const ENCODERS = {
   jpeg: MozJpegEncoder
 };
 
+type Image = {
+  type: ImageType,
+  options: CodecOptions,
+  data?: ImageBitmap,
+  counter: number,
+  loading: boolean
+};
+
 type Props = {};
 
 type State = {
+  sourceFile?: File,
   sourceImg?: ImageBitmap,
   sourceData?: ImageData,
-  leftImg?: ImageBitmap,
-  rightImg?: ImageBitmap,
-  loading: boolean
-  leftType: ImageType,
-  rightType: ImageType,
-  leftOptions: CodecOptions,
-  rightOptions: CodecOptions
+  images: Array<Image>,
+  loading: boolean,
+  error?: string
 };
 
 export default class App extends Component<Props, State> {
   state: State = {
     loading: false,
-    leftType: 'original',
-    rightType: 'jpeg',
-    leftOptions: {},
-    rightOptions: {}
+    images: [
+      { type: 'original', options: {}, loading: false, counter: 0 },
+      { type: 'jpeg', options: {}, loading: false, counter: 0 }
+    ]
   };
 
   optionsUpdateTimer?: NodeJS.Timer | number | null;
@@ -50,78 +55,68 @@ export default class App extends Component<Props, State> {
     // In development, persist application state across hot reloads:
     if (process.env.NODE_ENV === 'development') {
       this.setState(window.STATE);
-      this.componentDidUpdate = () => {
+      let oldCDU = this.componentDidUpdate;
+      this.componentDidUpdate = (props, state) => {
+        if (oldCDU) oldCDU.call(this, props, state);
         window.STATE = this.state;
       };
     }
   }
 
-  getEncoderName(type: ImageType, options: CodecOptions) {
-    const name = EncoderNames[type];
-    if (typeof name === 'function') return name(options);
-    return name;
+  setImageTypeAndOptions(index: number, type?: ImageType, options: any = {}) {
+    const images = this.state.images.slice();
+    const image = images[index];
+    images[index] = {
+      ...image,
+      type: type || image.type,
+      options,
+      loading: false,
+      counter: image.counter + 1
+    };
+    this.setState({ images });
   }
 
-  @bind
-  setLeftType(leftType: ImageType) {
-    this.setState({ leftType, leftOptions: {} });
-    this.optionsUpdated();
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    const { sourceImg, sourceData, images } = this.state;
+    for (let i = 0; i < images.length; i++) {
+      if (sourceData !== prevState.sourceData || images[i] !== prevState.images[i]) {
+        this.updateImage(i);
   }
-
-  @bind
-  setRightType(rightType: ImageType) {
-    this.setState({ rightType, rightOptions: {} });
-    this.optionsUpdated();
-  }
-
-  @bind
-  setLeftOptions(leftOptions: any) {
-    this.setState({ leftOptions });
-    this.optionsUpdated();
-  }
-
-  @bind
-  setRightOptions(rightOptions: any) {
-    this.setState({ rightOptions });
-    this.optionsUpdated();
-  }
-
-  optionsUpdated() {
-    if (!this.optionsUpdateTimer) {
-      this.optionsUpdateTimer = setTimeout(this.commitOptionsUpdated, 500);
     }
+    if (sourceImg !== prevState.sourceImg && prevState.sourceImg) {
+      prevState.sourceImg.close();
   }
-
-  @bind
-  commitOptionsUpdated() {
-    this.optionsUpdateTimer = null;
-    this.updateImages();
   }
 
   @bind
   async onFileChange(event: Event) {
-    this.setState({ loading: true });
     const fileInput = event.target as HTMLInputElement;
-    if (!fileInput.files || !fileInput.files[0]) return;
-    // TODO: handle decode error
-    const sourceImg = await createImageBitmap(fileInput.files[0]);
+    const sourceFile = fileInput.files && fileInput.files[0];
+    if (!sourceFile) return;
+    this.setState({ loading: true });
+    try {
+      const sourceImg = await createImageBitmap(sourceFile);
     // compute the corresponding ImageData once since it only changes when the file changes:
     const sourceData = await bitmapToImageData(sourceImg);
-    this.setState({ sourceImg, sourceData, loading: false });
-    this.updateImages();
+      this.setState({ sourceFile, sourceImg, sourceData, error: undefined, loading: false });
+    } catch (err) {
+      this.setState({ error: 'IMAGE_INVALID', loading: false });
+    }
   }
 
-  async updateImages() {
-    const { sourceData, leftType, rightType, leftOptions, rightOptions } = this.state;
+  async updateImage(index: number) {
+    const { sourceData, images } = this.state;
     if (!sourceData) return;
-    const id = ++this.compressCounter;
-    this.setState({ loading: true });
-    const [leftImg, rightImg] = await Promise.all([
-      this.updateCompressedImage(sourceData, leftType, leftOptions),
-      this.updateCompressedImage(sourceData, rightType, rightOptions)
-    ]);
-    if (this.compressCounter !== id) return;
-    this.setState({ leftImg, rightImg, loading: false });
+    let image = images[index];
+    const id = ++image.counter;
+    image.loading = true;
+    this.setState({ });
+    let result = await this.updateCompressedImage(sourceData, image.type, image.options);
+    image = this.state.images[index];
+    if (image.counter !== id) return;
+    image.data = result;
+    image.loading = false;
+    this.setState({ });
   }
 
   async updateCompressedImage(sourceData: ImageData, type: ImageType, options: CodecOptions) {
@@ -141,12 +136,16 @@ export default class App extends Component<Props, State> {
       }
     }
 
+  render({ }: Props, { loading, error, images }: State) {
+    for (let image of images) {
+      if (image.loading) loading = true;
   }
+    const leftImg = images[0].data;
+    const rightImg = images[1].data;
 
-  render({ }: Props, { loading, leftType, leftOptions, rightType, rightOptions, leftImg, rightImg }: State) {
     return (
       <div id="app" class={style.app}>
-        {leftImg && rightImg ? (
+        {(leftImg && rightImg) ? (
           <Output leftImg={leftImg} rightImg={rightImg} />
         ) : (
             <div class={style.welcome}>
@@ -154,25 +153,21 @@ export default class App extends Component<Props, State> {
               <input type="file" onChange={this.onFileChange} />
             </div>
           )}
-        <span class={style.leftLabel}>{this.getEncoderName(leftType, leftOptions)}</span>
-        <span class={style.rightLabel}>{this.getEncoderName(rightType, rightOptions)}</span>
+        {images.map((image, index: number) => (
+          <span class={index ? style.rightLabel : style.leftLabel}>{ENCODER_NAMES[image.type]}</span>
+        ))}
+        {images.map((image, index: number) => (
         <Options
-          class={style.leftOptions}
-          name="Left"
-          type={leftType}
-          options={leftOptions}
-          onTypeChange={this.setLeftType}
-          onOptionsChange={this.setLeftOptions}
+            class={index ? style.rightOptions : style.leftOptions}
+            name={index ? 'Right' : 'Left'}
+            type={image.type}
+            options={image.options}
+            onTypeChange={this.setImageTypeAndOptions.bind(this, index)}
+            onOptionsChange={this.setImageTypeAndOptions.bind(this, index, null)}
         />
-        <Options
-          class={style.rightOptions}
-          name="Right"
-          type={rightType}
-          options={rightOptions}
-          onTypeChange={this.setRightType}
-          onOptionsChange={this.setRightOptions}
-        />
+        ))}
         {loading && <span style={{ position: 'fixed', top: 0, left: 0 }}>Loading...</span>}
+        {error && <span style={{ position: 'fixed', top: 0, left: 0 }}>Error: {error}</span>}
       </div>
     );
   }
