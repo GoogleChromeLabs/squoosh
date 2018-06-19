8e7d4687745868e907c8db302f40c9cee1291476
Jason Miller <jason@developit.ca>
2018-06-19 20:46:20 +0000

    Formatting fixes for Options

--- src/components/options/index.tsx
+++ src/components/options/index.tsx
@@ -16,20 +16,20 @@ type ChildProps = {
 };
 
 CodecConfigurations.jpeg = ({ options, updateOption }: ChildProps) => (
-    <div>
-        <label>
-            Quality:
-            <input
-                name="quality"
-                type="range"
-                min="1"
-                max="100"
-                step="1"
-                value={options.quality}
-                onInput={updateOption}
-            />
-        </label>
-    </div>
+  <div>
+    <label>
+      Quality:
+      <input
+        name="quality"
+        type="range"
+        min="1"
+        max="100"
+        step="1"
+        value={options.quality}
+        onInput={updateOption}
+      />
+    </label>
+  </div>
 );
 
 type Props = {
@@ -57,19 +57,19 @@ export default class Options extends Component<Props, State> {
   }
 
   @bind
-    setType(e: Event) {
+  setType(e: Event) {
     const el = e.currentTarget as HTMLSelectElement;
     this.props.onTypeChange(el.value);
   }
 
   @bind
-    updateOption(e: Event) {
+  updateOption(e: Event) {
     const el = e.currentTarget as HTMLInputElement;
     this.setOption(el.name, /(rad|box)/i.test(el.type) ? el.checked : el.value);
   }
 
   @bind
-    setOption(key: string, value?: any) {
+  setOption(key: string, value?: any) {
     const options = Object.assign({}, this.state.options);
     options[key] = value;
     this.setState({ options });
@@ -80,23 +80,22 @@ export default class Options extends Component<Props, State> {
     const CodecOptionComponent = CodecConfigurations[type];
 
     return (
-            <div class={`${style.options}${c ? (' ' + c) : ''}`}>
-                <label>
-                    Mode:
-                    <select value={type} onChange={this.setType}>
-                        <option value="original">Original</option>
-                        <option value="jpeg">JPEG</option>
-                    </select>
-                </label>
-                {CodecOptionComponent && (
-                    <CodecOptionComponent
-                        options={options}
-                        setOption={this.setOption}
-                        updateOption={this.updateOption}
-                    />
-                )}
-                {/* <pre>{JSON.stringify(options,null,2)}</pre> */}
-            </div>
+      <div class={`${style.options}${c ? (' ' + c) : ''}`}>
+        <label>
+          Mode:
+          <select value={type} onChange={this.setType}>
+            <option value="original">Original</option>
+            <option value="jpeg">JPEG</option>
+          </select>
+        </label>
+        {CodecOptionComponent && (
+          <CodecOptionComponent
+            options={options}
+            setOption={this.setOption}
+            updateOption={this.updateOption}
+          />
+        )}
+      </div>
     );
   }
 }
