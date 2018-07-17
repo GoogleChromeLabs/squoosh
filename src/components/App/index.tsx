569d278b3904abf26ede61050553d2f2b411e11e
Jason Miller <developit@google.com>
2018-07-17 12:51:29 -0400

    Remove undefined initialization

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -105,7 +105,7 @@ export default class App extends Component<Props, State> {
     ],
   };
 
-  private snackbar?: SnackBarElement = undefined;
+  private snackbar?: SnackBarElement;
 
   constructor() {
     super();
