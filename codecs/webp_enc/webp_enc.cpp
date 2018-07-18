474e8fcd5ebc8a5ff8629016edb96592b35a0fbd
Jake Archibald <jaffathecake@gmail.com>
2018-07-18 17:01:44 +0100

    Holy shit struct binding

--- codecs/webp_enc/webp_enc.cpp
+++ codecs/webp_enc/webp_enc.cpp
@@ -9,19 +9,19 @@ int version() {
   return WebPGetEncoderVersion();
 }
 
-uint8_t* create_buffer(int width, int height) {
-  return (uint8_t*) malloc(width * height * 4 * sizeof(uint8_t));
+int create_buffer(int width, int height) {
+  return (int) malloc(width * height * 4 * sizeof(uint8_t));
 }
 
-void destroy_buffer(uint8_t* p) {
-  free(p);
+void destroy_buffer(int p) {
+  free((uint8_t*) p);
 }
 
 int result[2];
-void encode(uint8_t* img_in, int width, int height, float quality) {
+void encode(int img_in, int width, int height, float quality) {
   uint8_t* img_out;
   size_t size;
-  size = WebPEncodeRGBA(img_in, width, height, width * 4, quality, &img_out);
+  size = WebPEncodeRGBA((uint8_t*) img_in, width, height, width * 4, quality, &img_out);
   result[0] = (int)img_out;
   result[1] = size;
 }
@@ -38,20 +38,30 @@ int get_result_size() {
   return result[1];
 }
 
-/*struct Vector3d
-{
-  double x;
-  double y;
-  double z;
+struct Foo {
+  int bar;
+  float world;
 };
-*/
+
+typedef Foo Foo;
+
+int blah(Foo foo) {
+  return foo.bar;
+}
 
 EMSCRIPTEN_BINDINGS(my_module) {
+  value_object<Foo>("Foo")
+    .field("bar", &Foo::bar)
+    .field("world", &Foo::world);
+
+  function("blah", &blah);
+
+
   function("version", &version);
   function("create_buffer", &create_buffer, allow_raw_pointers());
   function("destroy_buffer", &destroy_buffer, allow_raw_pointers());
   function("encode", &encode, allow_raw_pointers());
-  function("free_result", &free_result, allow_raw_pointers());
+  function("free_result", &free_result);
   function("get_result_pointer", &get_result_pointer, allow_raw_pointers());
   function("get_result_size", &get_result_size, allow_raw_pointers());
 }
