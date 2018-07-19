ae375308decb59ccb7eb9cbd1e5fa0b695538a05
Jake Archibald <jaffathecake@gmail.com>
2018-07-19 18:50:19 +0100

    Options in the encoder!

--- codecs/webp_enc/webp_enc.cpp
+++ codecs/webp_enc/webp_enc.cpp
@@ -18,12 +18,32 @@ void destroy_buffer(int p) {
 }
 
 int result[2];
-void encode(int img_in, int width, int height, float quality) {
-  uint8_t* img_out;
-  size_t size;
-  size = WebPEncodeRGBA((uint8_t*) img_in, width, height, width * 4, quality, &img_out);
-  result[0] = (int)img_out;
-  result[1] = size;
+void encode(int img_in, int width, int height, WebPConfig config) {
+  // A lot of this is duplicated from Encode in picture_enc.c
+  WebPPicture pic;
+  WebPMemoryWriter wrt;
+  int ok;
+
+  if (!WebPPictureInit(&pic)) {
+    return;  // shouldn't happen, except if system installation is broken
+  }
+
+  pic.use_argb = !!config.lossless;
+  pic.width = width;
+  pic.height = height;
+  pic.writer = WebPMemoryWrite;
+  pic.custom_ptr = &wrt;
+
+  WebPMemoryWriterInit(&wrt);
+
+  ok = WebPPictureImportRGBA(&pic, (uint8_t*) img_in, width * 4) && WebPEncode(&config, &pic);
+  WebPPictureFree(&pic);
+  if (!ok) {
+    WebPMemoryWriterClear(&wrt);
+    return;
+  }
+  result[0] = (int)wrt.mem;
+  result[1] = wrt.size;
 }
 
 void free_result() {
@@ -38,24 +58,37 @@ int get_result_size() {
   return result[1];
 }
 
-struct Foo {
-  int bar;
-  float world;
-};
-
-typedef Foo Foo;
-
-int blah(Foo foo) {
-  return foo.bar;
-}
-
 EMSCRIPTEN_BINDINGS(my_module) {
-  value_object<Foo>("Foo")
-    .field("bar", &Foo::bar)
-    .field("world", &Foo::world);
-
-  function("blah", &blah);
-
+  value_object<WebPConfig>("WebPConfig")
+    .field("lossless", &WebPConfig::lossless)
+    .field("quality", &WebPConfig::quality)
+    .field("method", &WebPConfig::method)
+    //.field("image_hint", &WebPConfig::image_hint)
+    .field("target_size", &WebPConfig::target_size)
+    .field("target_PSNR", &WebPConfig::target_PSNR)
+    .field("segments", &WebPConfig::segments)
+    .field("sns_strength", &WebPConfig::sns_strength)
+    .field("filter_strength", &WebPConfig::filter_strength)
+    .field("filter_sharpness", &WebPConfig::filter_sharpness)
+    .field("filter_type", &WebPConfig::filter_type)
+    .field("autofilter", &WebPConfig::autofilter)
+    .field("alpha_compression", &WebPConfig::alpha_compression)
+    .field("alpha_filtering", &WebPConfig::alpha_filtering)
+    .field("alpha_quality", &WebPConfig::alpha_quality)
+    .field("pass", &WebPConfig::pass)
+    .field("show_compressed", &WebPConfig::show_compressed)
+    .field("preprocessing", &WebPConfig::preprocessing)
+    .field("partitions", &WebPConfig::partitions)
+    .field("partition_limit", &WebPConfig::partition_limit)
+    .field("emulate_jpeg_size", &WebPConfig::emulate_jpeg_size)
+    .field("thread_level", &WebPConfig::thread_level)
+    .field("low_memory", &WebPConfig::low_memory)
+    .field("near_lossless", &WebPConfig::near_lossless)
+    .field("exact", &WebPConfig::exact)
+    .field("use_delta_palette", &WebPConfig::use_delta_palette)
+    .field("use_sharp_yuv", &WebPConfig::use_sharp_yuv)
+    //.field("pad", &WebPConfig::pad)
+    ;
 
   function("version", &version);
   function("create_buffer", &create_buffer, allow_raw_pointers());
