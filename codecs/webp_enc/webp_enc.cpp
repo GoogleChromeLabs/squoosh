f2f467ecb8adfce17eb3b4fafc9ec9c8d8dc106f
Jake Archibald <jaffathecake@gmail.com>
2018-07-27 14:06:45 +0100

    WebP encode options (#110)
    * Flailing
    
    * Holy shit struct binding
    
    * Options in the encoder!
    
    * Integrating webp options
    
    * Addressing feedback
    
    * This isn't needed anymore

---
+++ codecs/webp_enc/webp_enc.cpp
@@ -0,0 +1,106 @@
+#include "emscripten.h"
+#include "src/webp/encode.h"
+#include <stdlib.h>
+#include <emscripten/bind.h>
+
+using namespace emscripten;
+
+int version() {
+  return WebPGetEncoderVersion();
+}
+
+int create_buffer(int width, int height) {
+  return (int) malloc(width * height * 4 * sizeof(uint8_t));
+}
+
+void destroy_buffer(int p) {
+  free((uint8_t*) p);
+}
+
+int result[2];
+void encode(int img_in, int width, int height, WebPConfig config) {
+  // A lot of this is duplicated from Encode in picture_enc.c
+  WebPPicture pic;
+  WebPMemoryWriter wrt;
+  int ok;
+
+  if (!WebPPictureInit(&pic)) {
+    return;  // shouldn't happen, except if system installation is broken
+  }
+
+  pic.use_argb = !!config.lossless;
+  pic.width = width;
+  pic.height = height;
+  pic.writer = WebPMemoryWrite;
+  pic.custom_ptr = &wrt;
+
+  WebPMemoryWriterInit(&wrt);
+
+  ok = WebPPictureImportRGBA(&pic, (uint8_t*) img_in, width * 4) && WebPEncode(&config, &pic);
+  WebPPictureFree(&pic);
+  if (!ok) {
+    WebPMemoryWriterClear(&wrt);
+    return;
+  }
+  result[0] = (int)wrt.mem;
+  result[1] = wrt.size;
+}
+
+void free_result() {
+  WebPFree((void*)result[0]);
+}
+
+int get_result_pointer() {
+  return result[0];
+}
+
+int get_result_size() {
+  return result[1];
+}
+
+EMSCRIPTEN_BINDINGS(my_module) {
+  enum_<WebPImageHint>("WebPImageHint")
+    .value("WEBP_HINT_DEFAULT", WebPImageHint::WEBP_HINT_DEFAULT)
+    .value("WEBP_HINT_PICTURE", WebPImageHint::WEBP_HINT_PICTURE)
+    .value("WEBP_HINT_PHOTO", WebPImageHint::WEBP_HINT_PHOTO)
+    .value("WEBP_HINT_GRAPH", WebPImageHint::WEBP_HINT_GRAPH)
+    ;
+
+  value_object<WebPConfig>("WebPConfig")
+    .field("lossless", &WebPConfig::lossless)
+    .field("quality", &WebPConfig::quality)
+    .field("method", &WebPConfig::method)
+    .field("image_hint", &WebPConfig::image_hint)
+    .field("target_size", &WebPConfig::target_size)
+    .field("target_PSNR", &WebPConfig::target_PSNR)
+    .field("segments", &WebPConfig::segments)
+    .field("sns_strength", &WebPConfig::sns_strength)
+    .field("filter_strength", &WebPConfig::filter_strength)
+    .field("filter_sharpness", &WebPConfig::filter_sharpness)
+    .field("filter_type", &WebPConfig::filter_type)
+    .field("autofilter", &WebPConfig::autofilter)
+    .field("alpha_compression", &WebPConfig::alpha_compression)
+    .field("alpha_filtering", &WebPConfig::alpha_filtering)
+    .field("alpha_quality", &WebPConfig::alpha_quality)
+    .field("pass", &WebPConfig::pass)
+    .field("show_compressed", &WebPConfig::show_compressed)
+    .field("preprocessing", &WebPConfig::preprocessing)
+    .field("partitions", &WebPConfig::partitions)
+    .field("partition_limit", &WebPConfig::partition_limit)
+    .field("emulate_jpeg_size", &WebPConfig::emulate_jpeg_size)
+    .field("thread_level", &WebPConfig::thread_level)
+    .field("low_memory", &WebPConfig::low_memory)
+    .field("near_lossless", &WebPConfig::near_lossless)
+    .field("exact", &WebPConfig::exact)
+    .field("use_delta_palette", &WebPConfig::use_delta_palette)
+    .field("use_sharp_yuv", &WebPConfig::use_sharp_yuv)
+    ;
+
+  function("version", &version);
+  function("create_buffer", &create_buffer, allow_raw_pointers());
+  function("destroy_buffer", &destroy_buffer, allow_raw_pointers());
+  function("encode", &encode, allow_raw_pointers());
+  function("free_result", &free_result);
+  function("get_result_pointer", &get_result_pointer, allow_raw_pointers());
+  function("get_result_size", &get_result_size, allow_raw_pointers());
+}
--- src/codecs/webp/Encoder.worker.ts
+++ src/codecs/webp/Encoder.worker.ts
@@ -1,22 +1,10 @@
-import webp_enc from '../../../codecs/webp_enc/webp_enc';
+import webp_enc, { WebPModule } from '../../../codecs/webp_enc/webp_enc';
 // Using require() so TypeScript doesn’t complain about this not being a module.
 import { EncodeOptions } from './encoder';
 const wasmBinaryUrl = require('../../../codecs/webp_enc/webp_enc.wasm');
 
-// API exposed by wasm module. Details in the codec’s README.
-interface ModuleAPI {
-  version(): number;
-  create_buffer(width: number, height: number): number;
-  destroy_buffer(pointer: number): void;
-  encode(buffer: number, width: number, height: number, quality: number): void;
-  free_result(): void;
-  get_result_pointer(): number;
-  get_result_size(): number;
-}
-
 export default class WebPEncoder {
-  private emscriptenModule: Promise<EmscriptenWasm.Module>;
-  private api: Promise<ModuleAPI>;
+  private emscriptenModule: Promise<WebPModule>;
 
   constructor() {
     this.emscriptenModule = new Promise((resolve) => {
@@ -41,38 +29,20 @@ export default class WebPEncoder {
         },
       });
     });
-
-    this.api = (async () => {
-      // Not sure why, but TypeScript complains that I am using
-      // `emscriptenModule` before it’s getting assigned, which is clearly not
-      // true :shrug: Using `any`
-      const module = await (this as any).emscriptenModule as EmscriptenWasm.Module;
-
-      return {
-        version: module.cwrap('version', 'number', []),
-        create_buffer: module.cwrap('create_buffer', 'number', ['number', 'number']),
-        destroy_buffer: module.cwrap('destroy_buffer', '', ['number']),
-        encode: module.cwrap('encode', '', ['number', 'number', 'number', 'number']),
-        free_result: module.cwrap('free_result', '', []),
-        get_result_pointer: module.cwrap('get_result_pointer', 'number', []),
-        get_result_size: module.cwrap('get_result_size', 'number', []),
-      };
-    })();
   }
 
   async encode(data: ImageData, options: EncodeOptions): Promise<ArrayBuffer> {
-    const m = await this.emscriptenModule;
-    const api = await this.api;
+    const module = await this.emscriptenModule;
 
-    const p = api.create_buffer(data.width, data.height);
-    m.HEAP8.set(data.data, p);
-    api.encode(p, data.width, data.height, options.quality);
-    const resultPointer = api.get_result_pointer();
-    const resultSize = api.get_result_size();
-    const resultView = new Uint8Array(m.HEAP8.buffer, resultPointer, resultSize);
+    const p = module.create_buffer(data.width, data.height);
+    module.HEAP8.set(data.data, p);
+    module.encode(p, data.width, data.height, options);
+    const resultPointer = module.get_result_pointer();
+    const resultSize = module.get_result_size();
+    const resultView = new Uint8Array(module.HEAP8.buffer, resultPointer, resultSize);
     const result = new Uint8Array(resultView);
-    api.free_result();
-    api.destroy_buffer(p);
+    module.free_result();
+    module.destroy_buffer(p);
 
     // wasm can’t run on SharedArrayBuffers, so we hard-cast to ArrayBuffer.
     return result.buffer as ArrayBuffer;
