ce48f77cd4f0171aabc6f61f4c80e5cdb82126d3
Surma <surma@surma.link>
2018-08-21 10:00:48 +0100

    Make WebP decoder use memory views

--- codecs/webp_dec/webp_dec.d.ts
+++ codecs/webp_dec/webp_dec.d.ts
@@ -1,1 +1,13 @@
-export default function(opts: EmscriptenWasm.ModuleOpts): EmscriptenWasm.Module;
+interface RawImage {
+  buffer: Uint8Array;
+  width: number;
+  height: number;
+}
+
+interface WebPModule extends EmscriptenWasm.Module {
+  decode(data: BufferSource): RawImage;
+  free_result(): void;
+}
+
+export default function(opts: EmscriptenWasm.ModuleOpts): WebPModule;
+
