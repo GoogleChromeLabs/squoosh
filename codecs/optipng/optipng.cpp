391da79ac4ba253479ff45d791952c375bbfb023
Surma <surma@surma.link>
2018-08-28 14:43:17 +0100

    example actually works

--- codecs/optipng/optipng.cpp
+++ codecs/optipng/optipng.cpp
@@ -5,7 +5,7 @@
 
 using namespace emscripten;
 
-extern "C" int process_files(int argc, char *argv[]);
+extern "C" int main(int argc, char *argv[]);
 
 int version() {
   // FIXME (@surma): Haven’t found a version in optipng :(
@@ -16,13 +16,15 @@ uint8_t* result;
 val compress(std::string png) {
   FILE* infile = fopen("input.png", "wb");
   fwrite(png.c_str(), png.length(), 1, infile);
+  fflush(infile);
   fclose(infile);
 
-  char* args[] = {"optipng", "-o7", "-zm1-9", "input.png", "-out", "output.png"};
-  process_files(6, args);
+  char* args[] = {"optipng", "-out", "output.png", "input.png"};
+  main(sizeof(args)/sizeof(args[0]), args);
 
   FILE *outfile = fopen("output.png", "rb");
-  int fsize = fseek(outfile, 0, SEEK_END);
+  fseek(outfile, 0, SEEK_END);
+  int fsize = ftell(outfile);
   result = (uint8_t*) malloc(fsize);
   fseek(outfile, 0, SEEK_SET);
   fread(result, fsize, 1, outfile);
@@ -34,6 +36,7 @@ void free_result() {
 }
 
 EMSCRIPTEN_BINDINGS(my_module) {
+  function("version", &version);
   function("compress", &compress);
   function("free_result", &free_result);
 }
