1b630a092fbae640adfdc0da232f934735567b39
Surma <surma@surmair.de>
2018-09-26 12:31:26 +0100

    Dockerize mozjpeg (#169)
    * Dockerize mozjpeg build process
    
    * Update README

---
+++ codecs/mozjpeg_enc/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+apt-get update
+apt-get install -qqy autoconf libtool libpng-dev pkg-config
+
+echo "============================================="
+echo "Compiling mozjpeg"
+echo "============================================="
+(
+  cd node_modules/mozjpeg
+  autoreconf -fiv
+  emconfigure ./configure --without-simd
+  emmake make libjpeg.la
+)
+echo "============================================="
+echo "Compiling mozjpeg done"
+echo "============================================="
+
+echo "============================================="
+echo "Compiling wasm bindings"
+echo "============================================="
+emcc \
+  --bind -O3 \
+  -s WASM=1 \
+  -s ALLOW_MEMORY_GROWTH=1 \
+  -s MODULARIZE=1 \
+  -s 'EXPORT_NAME="mozjpeg_enc"' \
+  -I node_modules/mozjpeg \
+  -o ./mozjpeg_enc.js \
+  -Wno-deprecated-register -Wno-writable-strings \
+  node_modules/mozjpeg/rdswitch.c \
+  -x c++ -std=c++11 \
+  mozjpeg_enc.cpp \
+  node_modules/mozjpeg/.libs/libjpeg.a
+echo "============================================="
+echo "Compiling wasm bindings done"
+echo "============================================="
