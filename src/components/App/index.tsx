5f53eeaf05d324fbc9cf2418f0b87e0a13d6efe8
Jake Archibald <jaffathecake@gmail.com>
2018-10-09 14:08:08 +0100

    Tidying JSX, and allowing image to render before first compression. Fixes #164.

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -335,35 +335,38 @@ export default class App extends Component<Props, State> {
     return (
       <file-drop accept="image/*" onfiledrop={this.onFileDrop}>
         <div id="app" class={`${style.app} ${style[orientation]}`}>
-          {(leftImageData && rightImageData && source) ? (
-            <Output
-              orientation={orientation}
-              imgWidth={source.data.width}
-              imgHeight={source.data.height}
-              leftImg={leftImageData}
-              rightImg={rightImageData}
-              leftImgContain={leftImage.preprocessorState.resize.fitMethod === 'cover'}
-              rightImgContain={rightImage.preprocessorState.resize.fitMethod === 'cover'}
-            />
-          ) : (
-            <Intro onFile={this.updateFile} />
-          )}
-          {(leftImageData && rightImageData && source) && images.map((image, index) => (
-            <Options
-              orientation={orientation}
-              sourceAspect={source.data.width / source.data.height}
-              imageIndex={index}
-              imageFile={image.file}
-              sourceImageFile={source && source.file}
-              downloadUrl={image.downloadUrl}
-              preprocessorState={image.preprocessorState}
-              encoderState={image.encoderState}
-              onEncoderTypeChange={this.onEncoderTypeChange.bind(this, index)}
-              onEncoderOptionsChange={this.onEncoderOptionsChange.bind(this, index)}
-              onPreprocessorOptionsChange={this.onPreprocessorOptionsChange.bind(this, index)}
-              onCopyToOtherClick={this.onCopyToOtherClick.bind(this, index)}
-            />
-          ))}
+          {source
+            ?
+              <div class={`${style.optionPair} ${style[orientation]}`}>
+                <Output
+                  orientation={orientation}
+                  imgWidth={source.data.width}
+                  imgHeight={source.data.height}
+                  leftImg={leftImageData || source.data}
+                  rightImg={rightImageData || source.data}
+                  leftImgContain={leftImage.preprocessorState.resize.fitMethod === 'cover'}
+                  rightImgContain={rightImage.preprocessorState.resize.fitMethod === 'cover'}
+                />
+                {images.map((image, index) => (
+                  <Options
+                    orientation={orientation}
+                    sourceAspect={source.data.width / source.data.height}
+                    imageIndex={index}
+                    imageFile={image.file}
+                    sourceImageFile={source && source.file}
+                    downloadUrl={image.downloadUrl}
+                    preprocessorState={image.preprocessorState}
+                    encoderState={image.encoderState}
+                    onEncoderTypeChange={this.onEncoderTypeChange.bind(this, index)}
+                    onEncoderOptionsChange={this.onEncoderOptionsChange.bind(this, index)}
+                    onPreprocessorOptionsChange={this.onPreprocessorOptionsChange.bind(this, index)}
+                    onCopyToOtherClick={this.onCopyToOtherClick.bind(this, index)}
+                  />
+                ))}
+              </div>
+            :
+              <Intro onFile={this.updateFile} />
+          }
           {anyLoading && <span style={{ position: 'fixed', top: 0, left: 0 }}>Loading...</span>}
           <snack-bar ref={linkRef(this, 'snackbar')} />
         </div>
