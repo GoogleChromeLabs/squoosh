f7a1961df18d90a2599c3354db032bb28fa6fbb5
Surma <surma@surma.link>
2018-10-11 12:39:55 +0100

    Update imagequant processor

---
+++ codecs/imagequant/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+export OPTIMIZE="-Os"
+export LDFLAGS="${OPTIMIZE}"
+export CFLAGS="${OPTIMIZE}"
+export CPPFLAGS="${OPTIMIZE}"
+
+echo "============================================="
+echo "Compiling libimagequant"
+echo "============================================="
+(
+  emcc \
+    --bind \
+    ${OPTIMIZE} \
+    -s ALLOW_MEMORY_GROWTH=1 \
+    -s MODULARIZE=1 \
+    -s 'EXPORT_NAME="imagequant"' \
+    -I node_modules/libimagequant \
+    --std=c99 \
+    -c \
+    node_modules/libimagequant/{libimagequant,pam,mediancut,blur,mempool,kmeans,nearest}.c
+)
+echo "============================================="
+echo "Compiling wasm module"
+echo "============================================="
+(
+  emcc \
+    --bind \
+    ${OPTIMIZE} \
+    -s ALLOW_MEMORY_GROWTH=1 \
+    -s MODULARIZE=1 \
+    -s 'EXPORT_NAME="imagequant"' \
+    -I node_modules/libimagequant \
+    -o ./imagequant.js \
+    --std=c++11 *.o \
+    -x c++ \
+    imagequant.cpp
+)
+echo "============================================="
+echo "Compiling wasm module done"
+echo "============================================="
