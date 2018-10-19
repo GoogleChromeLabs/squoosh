0ba3c57f10d69f2e9bec3798f39b3d3d026f5bbc
Jake Archibald <jaffathecake@gmail.com>
2018-10-19 08:42:51 +0100

    Generic options

--- src/codecs/browser-jpeg/encoder-meta.ts
+++ src/codecs/browser-jpeg/encoder-meta.ts
@@ -5,4 +5,4 @@ export const type = 'browser-jpeg';
 export const label = 'Browser JPEG';
 export const mimeType = 'image/jpeg';
 export const extension = 'jpg';
-export const defaultOptions: EncodeOptions = { quality: 0.5 };
+export const defaultOptions: EncodeOptions = { quality: 0.75 };
--- src/codecs/browser-webp/encoder-meta.ts
+++ src/codecs/browser-webp/encoder-meta.ts
@@ -7,5 +7,5 @@ export const type = 'browser-webp';
 export const label = 'Browser WebP';
 export const mimeType = 'image/webp';
 export const extension = 'webp';
-export const defaultOptions: EncodeOptions = { quality: 0.5 };
+export const defaultOptions: EncodeOptions = { quality: 0.75 };
 export const featureTest = () => canvasEncodeTest(mimeType);
