7a08815bcfcae66bb69a98735388882809b0ee0b
Surma <surma@surma.link>
2018-11-02 18:34:23 +0000

    Make emscripten with threads compile

--- codecs/webp_enc/build.sh
+++ codecs/webp_enc/build.sh
@@ -14,14 +14,12 @@ echo "============================================="
   emcc \
     ${OPTIMIZE} \
     --bind \
-    -D WEBP_USE_THREAD \
-    -s ALLOW_MEMORY_GROWTH=1 \
+    -D WEBP_USE_THREAD=1 \
     -s USE_PTHREADS=1 \
     -s ASSERTIONS=1 \
-    -s PTHREAD_POOL_SIZE=2 \
+    -s PTHREAD_POOL_SIZE=4 \
     -s TOTAL_MEMORY=268435456 \
     -s WASM_MEM_MAX=268435456 \
-    -s 'EXPORT_NAME="webp_enc"' \
     --std=c++11 \
     -I node_modules/libwebp \
     -o ./webp_enc.js \
