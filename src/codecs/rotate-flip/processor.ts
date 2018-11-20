276e1c897aa6cda516834f8050542de59da1cab1
Jake Archibald <jaffathecake@gmail.com>
2018-11-20 12:13:58 +0000

    Removing generator. Huge perf boost.

--- src/codecs/rotate-flip/processor.ts
+++ src/codecs/rotate-flip/processor.ts
@@ -2,55 +2,60 @@ import { RotateFlipOptions } from './processor-meta';
 
 const bpp = 4;
 
-function* iteratePixels(
-  width: number, height: number, opts: RotateFlipOptions,
-): IterableIterator<number> {
+export function rotateFlip(data: ImageData, opts: RotateFlipOptions): ImageData {
+  const flipDimensions = opts.rotate % 180 !== 0;
+  const width = flipDimensions ? data.height : data.width;
+  const height = flipDimensions ? data.width : data.height;
+  const out = new ImageData(width, height);
+  const { width: inputWidth, height: inputHeight } = data;
+  let i = 0;
+
   // In the straight-copy case, d1 is x, d2 is y.
   // x starts at 0 and increases.
   // y starts at 0 and increases.
   let d1Start = 0;
-  let d1Limit = width;
+  let d1Limit = inputWidth;
   let d1Advance = 1;
   let d1Multiplier = 1;
   let d2Start = 0;
-  let d2Limit = height;
+  let d2Limit = inputHeight;
   let d2Advance = 1;
-  let d2Multiplier = width;
+  let d2Multiplier = inputWidth;
 
   if (opts.rotate === 90) {
     // d1 is y, d2 is x.
     // y starts at its max value and decreases.
     // x starts at 0 and increases.
-    d1Start = height - 1;
-    d1Limit = height;
+    d1Start = inputHeight - 1;
+    d1Limit = inputHeight;
     d1Advance = -1;
-    d1Multiplier = width;
+    d1Multiplier = inputWidth;
     d2Start = 0;
-    d2Limit = width;
+    d2Limit = inputWidth;
     d2Advance = 1;
     d2Multiplier = 1;
   } else if (opts.rotate === 180) {
     // d1 is x, d2 is y.
     // x starts at its max and decreases.
     // y starts at its max and decreases.
-    d1Start = width - 1;
-    d1Limit = width;
+    d1Start = inputWidth - 1;
+    d1Limit = inputWidth;
     d1Advance = -1;
     d1Multiplier = 1;
-    d2Start = height - 1;
-    d2Limit = height;
+    d2Start = inputHeight - 1;
+    d2Limit = inputHeight;
     d2Advance = -1;
-    d2Multiplier = width;
+    d2Multiplier = inputWidth;
   } else if (opts.rotate === 270) {
     // d1 is y, d2 is x.
     // y starts at 0 and increases.
     // x starts at its max and decreases.
     d1Start = 0;
-    d1Limit = height;
+    d1Limit = inputHeight;
     d1Advance = 1;
-    d1Multiplier = width;
-    d2Start = width - 1;
-    d2Limit = width;
+    d1Multiplier = inputWidth;
+    d2Start = inputWidth - 1;
+    d2Limit = inputWidth;
     d2Advance = -1;
     d2Multiplier = 1;
   }
@@ -77,22 +82,11 @@ function* iteratePixels(
 
   for (let d2 = d2Start; d2 >= 0 && d2 < d2Limit; d2 += d2Advance) {
     for (let d1 = d1Start; d1 >= 0 && d1 < d1Limit; d1 += d1Advance) {
-      yield ((d1 * d1Multiplier) + (d2 * d2Multiplier)) * bpp;
-    }
-  }
-}
-
-export function rotateFlip(data: ImageData, opts: RotateFlipOptions): ImageData {
-  const flipDimensions = opts.rotate % 180 !== 0;
-  const width = flipDimensions ? data.height : data.width;
-  const height = flipDimensions ? data.width : data.height;
-  const out = new ImageData(width, height);
-  let i = 0;
-
-  for (const start of iteratePixels(data.width, data.height, opts)) {
-    for (let j = 0; j < 4; j += 1) {
-      out.data[i] = data.data[start + j];
-      i += 1;
+      const start = ((d1 * d1Multiplier) + (d2 * d2Multiplier)) * bpp;
+      for (let j = 0; j < 4; j += 1) {
+        out.data[i] = data.data[start + j];
+        i += 1;
+      }
     }
   }
 
