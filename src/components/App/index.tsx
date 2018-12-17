e67d50c8e6ca152e4faed47427215a744bc21dd1
Jake Archibald <jaffathecake@gmail.com>
2018-12-17 17:05:41 +0000

    Preventing zoom in iOS Safari. (#395)

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -62,6 +62,14 @@ export default class App extends Component<Props, State> {
       };
     }
 
+    // Since iOS 10, Apple tries to prevent disabling pinch-zoom. This is great in theory, but
+    // really breaks things on Squoosh, as you can easily end up zooming the UI when you mean to
+    // zoom the image. Once you've done this, it's really difficult to undo. Anyway, this seems to
+    // prevent it.
+    document.body.addEventListener('gesturestart', (event) => {
+      event.preventDefault();
+    });
+
     window.addEventListener('popstate', this.onPopState);
   }
 
