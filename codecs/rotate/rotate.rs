436faa17af652417b75582b1b4d7a5c4a5f85bfc
Surma <surma@surma.link>
2019-02-19 11:28:01 +0000

    More conservative tiling size

--- codecs/rotate/rotate.rs
+++ codecs/rotate/rotate.rs
@@ -22,7 +22,7 @@ impl<T> HardUnwrap<T> for Option<T> {
     }
 }
 
-const TILE_SIZE: usize = 64;
+const TILE_SIZE: usize = 16;
 
 #[no_mangle]
 fn rotate(width: usize, height: usize, rotate: usize) {
