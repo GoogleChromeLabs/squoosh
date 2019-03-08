75431640c0ae715f5358d7615ad37cfabc90917c
Surma <surma@surma.link>
2019-03-08 11:02:55 +0000

    Add comment about division by zero

--- codecs/resize/src/lib.rs
+++ codecs/resize/src/lib.rs
@@ -28,7 +28,7 @@ pub fn resize(
     output_width: usize,
     output_height: usize,
     typ_idx: usize,
-    premultiply: bool
+    premultiply: bool,
 ) -> Vec<u8> {
     let typ = match typ_idx {
         0 => Type::Triangle,
@@ -41,11 +41,13 @@ pub fn resize(
     let num_output_pixels = output_width * output_height;
 
     if premultiply {
-      for i in 0..num_input_pixels {
-        for j in 0..3 {
-          input_image[4 * i + j] = ((input_image[4 * i + j] as f32) * (input_image[4 * i + 3] as f32)/255.0) as u8;
+        for i in 0..num_input_pixels {
+            for j in 0..3 {
+                input_image[4 * i + j] = ((input_image[4 * i + j] as f32)
+                    * (input_image[4 * i + 3] as f32)
+                    / 255.0) as u8;
+            }
         }
-      }
     }
 
     let mut resizer = resize::new(
@@ -61,13 +63,17 @@ pub fn resize(
     resizer.resize(input_image.as_slice(), output_image.as_mut_slice());
 
     if premultiply {
-      for i in 0..num_output_pixels {
-        for j in 0..3 {
-          output_image[4 * i + j] = ((output_image[4 * i + j] as f32) * 255.0 / (output_image[4 * i + 3] as f32)) as u8;
+        for i in 0..num_output_pixels {
+            for j in 0..3 {
+                // We don’t need to worry about division by zero, as division by zero
+                // is well-defined on floats to return `±Inf`. ±Inf is converted to 0
+                // when casting to integers.
+                output_image[4 * i + j] = ((output_image[4 * i + j] as f32) * 255.0
+                    / (output_image[4 * i + 3] as f32))
+                    as u8;
+            }
         }
-      }
     }
-
 
     return output_image;
 }
