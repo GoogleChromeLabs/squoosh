ac9a7767d22ff58cfb839601fd6996eae76f6665
Surma <surma@surma.link>
2020-02-06 09:21:43 -0800

    Expose some options for AVIF

--- codecs/avif_enc/avif_enc.cpp
+++ codecs/avif_enc/avif_enc.cpp
@@ -4,10 +4,41 @@
 
 using namespace emscripten;
 
+struct AvifOptions {
+  // [0 - 63]
+  // 0 = lossless
+  // 63 = worst quality
+  int minQuantizer;
+  int maxQuantizer;
+  // [0 - 6]
+  // Creates 2^n tiles in that dimension
+  int tileRowsLog2;
+  int tileColsLog2;
+  // [0 - 10]
+  // 0 = slowest
+  // 10 = fastest
+  int speed;
+  // 0 = 4:2:0
+  // 1 = 4:2:2
+  // 2 = 4:4:4
+  int subsample;
+};
+
 avifRWData output = AVIF_DATA_EMPTY;
-val encode(std::string buffer, int width, int height) {
+val encode(std::string buffer, int width, int height, AvifOptions options) {
   int depth = 8;
-  avifPixelFormat format = AVIF_PIXEL_FORMAT_YUV420;
+  avifPixelFormat format;
+  switch (options.subsample) {
+  case 0:
+    format = AVIF_PIXEL_FORMAT_YUV420;
+    break;
+  case 1:
+    format = AVIF_PIXEL_FORMAT_YUV422;
+    break;
+  case 2:
+    format = AVIF_PIXEL_FORMAT_YUV444;
+    break;
+  }
   avifImage *image = avifImageCreate(width, height, depth, format);
 
   uint8_t *rgba = (uint8_t *)buffer.c_str();
@@ -27,15 +58,15 @@ val encode(std::string buffer, int width, int height) {
 
   avifEncoder *encoder = avifEncoderCreate();
   encoder->maxThreads = 1;
-  encoder->minQuantizer = AVIF_QUANTIZER_LOSSLESS;
-  encoder->maxQuantizer = AVIF_QUANTIZER_LOSSLESS;
+  encoder->minQuantizer = options.minQuantizer;
+  encoder->maxQuantizer = options.maxQuantizer;
+  encoder->tileRowsLog2 = options.tileRowsLog2;
+  encoder->tileColsLog2 = options.tileColsLog2;
+  encoder->speed = options.speed;
   avifResult encodeResult = avifEncoderWrite(encoder, image, &output);
   if (encodeResult != AVIF_RESULT_OK) {
     return val::null();
   }
-  // output contains a valid .avif file's contents
-  // ... output.data;
-  // ... output.size;
   avifImageDestroy(image);
   avifEncoderDestroy(encoder);
   return val(typed_memory_view(output.size, output.data));
@@ -44,6 +75,14 @@ val encode(std::string buffer, int width, int height) {
 void free_result() { avifRWDataFree(&output); }
 
 EMSCRIPTEN_BINDINGS(my_module) {
+  value_object<AvifOptions>("AvifOptions")
+      .field("minQuantizer", &AvifOptions::minQuantizer)
+      .field("maxQuantizer", &AvifOptions::maxQuantizer)
+      .field("tileRowsLog2", &AvifOptions::tileRowsLog2)
+      .field("tileColsLog2", &AvifOptions::tileColsLog2)
+      .field("speed", &AvifOptions::speed)
+      .field("subsample", &AvifOptions::subsample);
+
   function("encode", &encode);
   function("free_result", &free_result);
 }
