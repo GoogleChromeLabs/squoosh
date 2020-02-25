570e604be024a0df9d85c5f46f10487c7e57a076
Ingvar Stepanyan <rreverser@google.com>
2020-02-25 18:35:17 +0000

    optipng: switch to bundled zlib and libpng
    Benefits:
     - newer versions of the libraries
        - zlib: 1.2.8 -> 1.2.11
    	- libpng: 1.6.18beta04 -> 1.6.34
     - much fewer dependencies to install (as libs are already in optipng archive and we don't need napa)
     - much smaller build thanks to customised versions of zlib and libpng containing only APIs necessary for optipng itself: 238950 -> 177359 bytes
     - much faster build thanks to preconfigured libpng and stripped APIs: 2m15s -> 40s
     - much simpler build script: 77 -> 46 lines

--- codecs/optipng/build.sh
+++ codecs/optipng/build.sh
@@ -4,46 +4,13 @@ set -e
 
 export OPTIMIZE="-Os"
 export PREFIX="/src/build"
-export CFLAGS="${OPTIMIZE} -I${PREFIX}/include/"
-export CPPFLAGS="${OPTIMIZE} -I${PREFIX}/include/"
-export LDFLAGS="${OPTIMIZE} -L${PREFIX}/lib/"
-
-apt-get update
-apt-get install -qqy autoconf libtool
-
-echo "============================================="
-echo "Compiling zlib"
-echo "============================================="
-test -n "$SKIP_ZLIB" || (
-  cd node_modules/zlib
-  emconfigure ./configure --prefix=${PREFIX}/
-  emmake make
-  emmake make install
-)
-echo "============================================="
-echo "Compiling zlib done"
-echo "============================================="
-
-echo "============================================="
-echo "Compiling libpng"
-echo "============================================="
-test -n "$SKIP_LIBPNG" || (
-  cd node_modules/libpng
-  autoreconf -i
-  emconfigure ./configure --with-zlib-prefix=${PREFIX}/ --prefix=${PREFIX}/
-  emmake make
-  emmake make install
-)
-echo "============================================="
-echo "Compiling libpng done"
-echo "============================================="
 
 echo "============================================="
 echo "Compiling optipng"
 echo "============================================="
 (
   cd node_modules/optipng
-  emconfigure ./configure --prefix=${PREFIX} --with-system-libs
+  CFLAGS="${OPTIMIZE} -Isrc/zlib" emconfigure ./configure --prefix=${PREFIX}
   emmake make
   emmake make install
   mkdir -p ${PREFIX}/lib
