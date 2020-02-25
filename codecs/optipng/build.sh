fce61c8c89e082502c4afaa34053c8f143444158
Ingvar Stepanyan <rreverser@google.com>
2020-02-25 17:14:16 +0000

    Switch to emscripten-upstream
    Before: 255184 bytes, 2m15s
    After: 238270 bytes, 2m6s

--- codecs/optipng/build.sh
+++ codecs/optipng/build.sh
@@ -83,5 +83,5 @@ echo "============================================="
 
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Did you update your docker image?"
-echo "Run \`docker pull trzeci/emscripten\`"
+echo "Run \`docker pull trzeci/emscripten-upstream\`"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
