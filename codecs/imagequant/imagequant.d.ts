1f35c40d3f3a82b62d591a86a6597fedc3fae0ee
Ingvar Stepanyan <rreverser@google.com>
2020-05-14 16:52:47 +0100

    Create Uint8ClampedArray from C++

--- codecs/imagequant/imagequant.d.ts
+++ codecs/imagequant/imagequant.d.ts
@@ -1,12 +1,6 @@
-interface RawImage {
-  buffer: Uint8Array;
-  width: number;
-  height: number;
-}
-
 interface QuantizerModule extends EmscriptenWasm.Module {
-  quantize(data: BufferSource, width: number, height: number, numColors: number, dither: number): RawImage;
-  zx_quantize(data: BufferSource, width: number, height: number, dither: number): RawImage;
+  quantize(data: BufferSource, width: number, height: number, numColors: number, dither: number): Uint8ClampedArray;
+  zx_quantize(data: BufferSource, width: number, height: number, dither: number): Uint8ClampedArray;
   free_result(ptr: number): void;
 }
 
