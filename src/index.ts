85eb94b725d01e241724a4d1bc28dc07a198677c
Pete LePage <pete.lepage@pobox.com>
2020-06-24 09:54:54 -0400

    Set the dimension value

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ if (typeof PRERENDER === 'undefined') {
   window.ga = window.ga || ((...args) => (ga.q = ga.q || []).push(args));
   ga('create', 'UA-128752250-1', 'auto');
   ga('set', 'transport', 'beacon');
-  ga('set', 'dimensionX', displayMode);
+  ga('set', 'dimension1', displayMode);
   ga('send', 'pageview');
   // Load the GA script
   const s = document.createElement('script');
