245527ae0ffa32aac0d39b9b53bb7f48c8a3afdb
Surma <surma@surma.dev>
2020-08-17 18:21:09 +0100

    Update for encoder API changes

--- codecs/jxl/enc/jxl_enc.d.ts
+++ codecs/jxl/enc/jxl_enc.d.ts
@@ -2,7 +2,6 @@ import { EncodeOptions } from '../../src/codecs/jxl/encoder-meta';
 
 interface JXLModule extends EmscriptenWasm.Module {
   encode(data: BufferSource, width: number, height: number, options: EncodeOptions): Uint8Array;
-  free_result(): void;
 }
 
 
