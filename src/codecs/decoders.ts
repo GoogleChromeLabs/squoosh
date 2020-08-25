3f57f9fef1aff83413936e8734e1f82d3b5ad0a6
Jake Archibald <jaffathecake@gmail.com>
2020-08-25 14:13:21 +0100

    Feature test decoding capability

--- src/codecs/decoders.ts
+++ src/codecs/decoders.ts
@@ -1,14 +1,16 @@
 import { builtinDecode, sniffMimeType, canDecodeImage } from '../lib/util';
 import Processor from './processor';
 import webpDataUrl from 'url-loader!./tiny.webp';
+import avifDataUrl from 'url-loader!./tiny.avif';
 
 const webPSupported = canDecodeImage(webpDataUrl);
+const avifSupported = canDecodeImage(avifDataUrl);
 
 export async function decodeImage(blob: Blob, processor: Processor): Promise<ImageData> {
   const mimeType = await sniffMimeType(blob);
 
   try {
-    if (mimeType === 'image/avif') {
+    if (mimeType === 'image/avif' && !(await avifSupported)) {
       return await processor.avifDecode(blob);
     }
     if (mimeType === 'image/webp' && !(await webPSupported)) {
