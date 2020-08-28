045cb14c54fb1f0e6424e54c0f8654ed12e680ef
Surma <surma@surma.dev>
2020-08-28 12:45:09 +0100

    Expose butteraugli distance

--- src/codecs/jxl/encoder-meta.ts
+++ src/codecs/jxl/encoder-meta.ts
@@ -1,14 +1,16 @@
 export interface EncodeOptions {
   speed: number;
+  quality: number;
 }
 
 export interface EncoderState { type: typeof type; options: EncodeOptions; }
 
 export const type = 'jxl';
-export const label = 'JPEG XL';
+export const label = 'JPEG XL (beta)';
 export const mimeType = 'image/jpegxl';
 export const extension = 'jxl';
 // These come from struct WebPConfig in encode.h.
 export const defaultOptions: EncodeOptions = {
   speed: 1,
+  quality: 0.5,
 };
