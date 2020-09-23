c754016ac7460667a15231a9bec638a390100598
Ingvar Stepanyan <rreverser@google.com>
2020-09-23 15:54:21 +0100

    Fixups

--- src/codecs/avif/encoder.ts
+++ src/codecs/avif/encoder.ts
@@ -1,21 +1,26 @@
 import mainUrl from 'file-loader!../../../codecs/avif/enc/avif_enc.js';
-import avif_enc, { AVIFModule } from '../../../codecs/avif/enc/avif_enc.js';
+import { AVIFModule } from '../../../codecs/avif/enc/avif_enc.js';
 import wasmUrl from '../../../codecs/avif/enc/avif_enc.wasm';
 import workerUrl from 'file-loader!../../../codecs/avif/enc/avif_enc.worker.js';
 import { EncodeOptions } from './encoder-meta';
 import { initEmscriptenModule } from '../util';
 
+declare const avif_enc: typeof import('../../../codecs/avif/enc/avif_enc.js').default;
+
 let emscriptenModule: Promise<AVIFModule>;
 
+async function init() {
+  importScripts(mainUrl as unknown as string);
+  return initEmscriptenModule<AVIFModule>(
+    avif_enc,
+    wasmUrl,
+    workerUrl,
+    mainUrl as unknown as string,
+  );
+}
+
 export async function encode(data: ImageData, options: EncodeOptions): Promise<ArrayBuffer> {
-  if (!emscriptenModule) {
-    emscriptenModule = initEmscriptenModule(
-      avif_enc,
-      wasmUrl,
-      workerUrl,
-      mainUrl as unknown as string,
-    );
-  }
+  if (!emscriptenModule) emscriptenModule = init();
 
   const module = await emscriptenModule;
   const result = module.encode(data.data, data.width, data.height, options);
