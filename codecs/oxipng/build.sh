684b041262cbecdb267788cf34445a0b1cff5270
Ingvar Stepanyan <rreverser@google.com>
2020-09-24 13:29:32 +0100

    Add feature detection to OxiPNG

--- codecs/oxipng/build.sh
+++ codecs/oxipng/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 rm -rf pkg,{-parallel}
-wasm-pack build -t web
+wasm-pack build
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory' wasm-pack build -t web -d pkg-parallel -- -Z build-std=panic_abort,std --features=parallel
 sed -i "s|input = import.meta.url.replace(/\\\.js$/, '_bg.wasm');||" pkg{,-parallel}/squoosh_oxipng.js
 rm pkg{,-parallel}/.gitignore
--- src/codecs/oxipng/encoder.ts
+++ src/codecs/oxipng/encoder.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import optimiser from '../../../codecs/oxipng/spawn';
+import optimiser from '../../../codecs/oxipng';
 import { EncodeOptions } from './encoder-meta';
 
 export async function compress(data: ArrayBuffer, options: EncodeOptions): Promise<ArrayBuffer> {
