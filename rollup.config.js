3f2466f44dc7488fe7a498397baf271853052186
Jake Archibald <jaffathecake@gmail.com>
2020-09-24 15:20:13 +0100

    Re-enable terser

--- rollup.config.js
+++ rollup.config.js
@@ -90,7 +90,7 @@ export default async function ({ watch }) {
             commonjs(),
             resolve(),
             replace({ __PRERENDER__: false, __PRODUCTION__: isProduction }),
-            //terser({ module: true }),
+            terser({ module: true }),
           ],
         },
         {
