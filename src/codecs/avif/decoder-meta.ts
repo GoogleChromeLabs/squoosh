b9e4fd889739da5354274cc30860ea41f7fbbf9b
pramodrajeshwankhade <56465246+pramodrajeshwankhade@users.noreply.github.com>
2020-10-01 22:11:44 +0530

    Update decoder-meta.ts

--- src/codecs/avif/decoder-meta.ts
+++ src/codecs/avif/decoder-meta.ts
@@ -2,6 +2,7 @@ export const name = 'WASM AVIF Decoder';
 
 const supportedMimeTypes = ['image/avif'];
 
-export function canHandleMimeType(mimeType: string): boolean {
+export function canHandleMimeType(mimeType: string): boolean 
+ {
   return supportedMimeTypes.includes(mimeType);
-}
+ }
