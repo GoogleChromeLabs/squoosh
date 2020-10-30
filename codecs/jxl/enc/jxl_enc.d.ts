e581075f649ad2e639e7929fa39d828e61f30dab
Ingvar Stepanyan <rreverser@google.com>
2020-10-30 14:35:17 +0000

    Fix types after rebase onto latest dev

--- codecs/jxl/enc/jxl_enc.d.ts
+++ codecs/jxl/enc/jxl_enc.d.ts
@@ -1,8 +1,7 @@
-import { EncodeOptions } from '../../src/codecs/jxl/encoder-meta';
+import { EncodeOptions } from '../../../src/codecs/jxl/encoder-meta';
 
 interface JXLModule extends EmscriptenWasm.Module {
-  encode(data: BufferSource, width: number, height: number, options: EncodeOptions): Uint8Array;
+  encode(data: BufferSource, width: number, height: number, options: EncodeOptions): Uint8Array | null;
 }
 
-
-export default function(opts: EmscriptenWasm.ModuleOpts): JXLModule;
+export default function(opts: EmscriptenWasm.ModuleOpts): Promise<JXLModule>;
