b412d4c2a56129c7a154568e60aa0d983fc21112
Jake Archibald <jaffathecake@gmail.com>
2020-11-11 15:59:29 +0000

    Moving component instantiation to avoid preact removing it

--- src/shared/initial-app/custom-els/loading-spinner/index.ts
+++ src/shared/initial-app/custom-els/loading-spinner/index.ts
@@ -18,38 +18,29 @@ const HTMLEl = ((__PRERENDER__
 export default class LoadingSpinner extends HTMLEl {
   private _delayTimeout: number = 0;
 
-  constructor() {
-    super();
-    if (!__PRERENDER__) return;
-
-    // Ideally we'd use shadow DOM here, but we're targeting browsers without shadow DOM support.
-    // You can't set attributes/content in a custom element constructor, so I'm waiting a microtask.
-    Promise.resolve().then(() => {
-      this.style.display = 'none';
-      // prettier-ignore
-      this.innerHTML = '' +
-        `<div class="${styles.spinnerContainer}">` +
-          `<div class="${styles.spinnerLayer}">` +
-            `<div class="${styles.spinnerCircleClipper} ${styles.spinnerLeft}">` +
-              `<div class="${styles.spinnerCircle}"></div>` +
-            '</div>' +
-            `<div class="${styles.spinnerGapPatch}">` +
-              `<div class="${styles.spinnerCircle}"></div>` +
-            '</div>' +
-            `<div class="${styles.spinnerCircleClipper} ${styles.spinnerRight}">` +
-              `<div class="${styles.spinnerCircle}"></div>` +
-            '</div>' +
-          '</div>' +
-        '</div>';
-    });
-  }
-
   disconnectedCallback() {
     this.style.display = 'none';
     clearTimeout(this._delayTimeout);
   }
 
   connectedCallback() {
+    this.style.display = 'none';
+    // prettier-ignore
+    this.innerHTML = '' +
+      `<div class="${styles.spinnerContainer}">` +
+        `<div class="${styles.spinnerLayer}">` +
+          `<div class="${styles.spinnerCircleClipper} ${styles.spinnerLeft}">` +
+            `<div class="${styles.spinnerCircle}"></div>` +
+          '</div>' +
+          `<div class="${styles.spinnerGapPatch}">` +
+            `<div class="${styles.spinnerCircle}"></div>` +
+          '</div>' +
+          `<div class="${styles.spinnerCircleClipper} ${styles.spinnerRight}">` +
+            `<div class="${styles.spinnerCircle}"></div>` +
+          '</div>' +
+        '</div>' +
+      '</div>';
+
     const delayStr = getComputedStyle(this).getPropertyValue('--delay').trim();
     let delayNum = parseFloat(delayStr);
 
