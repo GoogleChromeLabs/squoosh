97919b9e5d152e59c68dbc1a14c5cf4b213c66a6
Ingvar Stepanyan <rreverser@google.com>
2020-11-16 15:54:09 +0000

    Build hqx with Rust 1.40; refactor build-rust*.sh

--- codecs/build-rust.sh
+++ codecs/build-rust.sh
@@ -1,4 +1,10 @@
 set -e
 
-docker build -t squoosh-rust - < ../rust.Dockerfile
-docker run --rm -v $PWD:/src squoosh-rust "$@"
+if [ ! -z "$RUST_IMG" ]
+then
+  # Get part after ":" (https://stackoverflow.com/a/15149278/439965).
+  IMG_SUFFIX=-${RUST_IMG#*:}
+fi
+IMG_NAME=squoosh-rust$IMG_SUFFIX
+docker build -t $IMG_NAME --build-arg RUST_IMG - < ../rust.Dockerfile
+docker run --rm -v $PWD:/src $IMG_NAME "$@"
