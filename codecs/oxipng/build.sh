2d00f6cc45db5ef5687234b375ff8286e0d8cc0c
Jake Archibald <jaffathecake@gmail.com>
2020-11-16 17:15:37 +0000

    Set web target

--- codecs/oxipng/build.sh
+++ codecs/oxipng/build.sh
@@ -3,6 +3,6 @@
 set -e
 
 rm -rf pkg,{-parallel}
-wasm-pack build
+wasm-pack build --target web
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory' wasm-pack build -t web -d pkg-parallel -- -Z build-std=panic_abort,std --features=parallel
 rm pkg{,-parallel}/.gitignore
