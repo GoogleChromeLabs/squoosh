ed7547d41dd989eceb2587458e40580bed7191df
Ingvar Stepanyan <rreverser@google.com>
2020-11-19 19:01:27 +0000

    Fix nightly Rust pinning; rebuild Oxi

--- codecs/oxipng/pkg-parallel/squoosh_oxipng.d.ts
+++ codecs/oxipng/pkg-parallel/squoosh_oxipng.d.ts
@@ -1,29 +1,24 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
- * @param {Uint8Array} data
- * @param {number} level
- * @returns {Uint8Array}
- */
+* @param {Uint8Array} data
+* @param {number} level
+* @returns {Uint8Array}
+*/
 export function optimise(data: Uint8Array, level: number): Uint8Array;
 /**
- * @param {number} num
- * @returns {any}
- */
+* @param {number} num
+* @returns {any}
+*/
 export function worker_initializer(num: number): any;
 /**
- */
+*/
 export function start_main_thread(): void;
 /**
- */
+*/
 export function start_worker_thread(): void;
 
-export type InitInput =
-  | RequestInfo
-  | URL
-  | Response
-  | BufferSource
-  | WebAssembly.Module;
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
   readonly optimise: (a: number, b: number, c: number, d: number) => void;
@@ -39,15 +34,13 @@ export interface InitOutput {
 }
 
 /**
- * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
- * for everything else, calls `WebAssembly.instantiate` directly.
- *
- * @param {InitInput | Promise<InitInput>} module_or_path
- * @param {WebAssembly.Memory} maybe_memory
- *
- * @returns {Promise<InitOutput>}
- */
-export default function init(
-  module_or_path?: InitInput | Promise<InitInput>,
-  maybe_memory?: WebAssembly.Memory,
-): Promise<InitOutput>;
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+* @param {WebAssembly.Memory} maybe_memory
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>, maybe_memory?: WebAssembly.Memory): Promise<InitOutput>;
+        
