e691860999ff5499da2cb4ba10ae1e9d8239c7e4
Ingvar Stepanyan <rreverser@google.com>
2020-11-19 10:15:42 +0000

    Rebuild with same nightly Rust

--- codecs/build-rust-nightly.sh
+++ codecs/build-rust-nightly.sh
@@ -1,4 +1,4 @@
 set -e
 
-docker build -t squoosh-rust-nightly --build-arg RUST_IMG=rustlang/rust:nightly - < ../rust.Dockerfile
+docker build -t squoosh-rust-nightly --build-arg RUST_IMG=rustlang/rust@sha256:a7e9ab157d7720536fd8e1db918dde49fb642f2b4db90f97cec2b8b6d6e4250b - < ../rust.Dockerfile
 docker run --rm -v $PWD:/src squoosh-rust-nightly "$@"
