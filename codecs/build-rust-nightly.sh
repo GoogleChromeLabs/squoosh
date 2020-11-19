dc2f2e48c92f680075a2f1b09d211af0fed6c80b
Ingvar Stepanyan <rreverser@google.com>
2020-11-19 12:17:08 +0000

    Build with older nightly

--- codecs/build-rust-nightly.sh
+++ codecs/build-rust-nightly.sh
@@ -1,4 +1,4 @@
 set -e
 
-docker build -t squoosh-rust-nightly --build-arg RUST_IMG=rustlang/rust@sha256:a7e9ab157d7720536fd8e1db918dde49fb642f2b4db90f97cec2b8b6d6e4250b - < ../rust.Dockerfile
+docker build -t squoosh-rust-nightly --build-arg RUST_IMG=rustlang/rust@sha256:744aeea5a38f95aa7a96ec37269a65f0c6197a1cdd87d6534e12bb869141d807 - < ../rust.Dockerfile
 docker run --rm -v $PWD:/src squoosh-rust-nightly "$@"
