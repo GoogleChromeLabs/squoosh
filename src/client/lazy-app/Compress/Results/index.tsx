0c0ed5e12931f9a45faddff042238d020d018328
Jake Archibald <jaffathecake@gmail.com>
2020-12-08 12:11:54 +0000

    Hook up download button

--- src/client/lazy-app/Compress/Results/index.tsx
+++ src/client/lazy-app/Compress/Results/index.tsx
@@ -106,10 +106,15 @@ export default class Results extends Component<Props, State> {
             </div>
           </div>
         </div>
-        <button
+        <a
           class={showLoadingState ? style.downloadDisable : style.download}
+          href={downloadUrl}
+          download={imageFile ? imageFile.name : ''}
+          title="Download"
+          onClick={this.onDownload}
         >
           <svg class={style.downloadBlobs} viewBox="0 0 89.6 86.9">
+            <title>Download</title>
             <path d="M27.3 72c-8-4-15.6-12.3-16.9-21-1.2-8.7 4-17.8 10.5-26s14.4-15.6 24-16 21.2 6 28.6 16.5c7.4 10.5 10.8 25 6.6 34S64.1 71.8 54 73.6c-10.2 2-18.7 2.3-26.7-1.6z" />
             <path d="M19.8 24.8c4.3-7.8 13-15 21.8-15.7 8.7-.8 17.5 4.8 25.4 11.8 7.8 6.9 14.8 15.2 14.7 24.9s-7.1 20.7-18 27.6c-10.8 6.8-25.5 9.5-34.2 4.8S18.1 61.6 16.7 51.4c-1.3-10.3-1.3-18.8 3-26.6z" />
           </svg>
@@ -117,7 +122,7 @@ export default class Results extends Component<Props, State> {
             <DownloadIcon />
           </div>
           {showLoadingState && <loading-spinner />}
-        </button>
+        </a>
       </div>
     );
   }
