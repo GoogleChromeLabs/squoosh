5765ea5aa4416753c5719828df3745946b3586a4
Surma <surma@surma.dev>
2020-12-09 10:16:02 +0000

    Update cli/src/image_data.js

--- cli/src/image_data.js
+++ cli/src/image_data.js
@@ -1,10 +1,5 @@
 export default class ImageData {
   constructor(data, width, height) {
-    // Need to manually copy the memory as wasm-bindgen does not by default
-    // and by the time we get control in JS land, the memory has already
-    // been corrupted.
-    // FIXME: This is bad because it’s overhead that we should only need
-    // to pay for Rust, not for C++.
     this.data = data;
     this.width = width;
     this.height = height;
