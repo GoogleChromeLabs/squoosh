cd4e9fd107aa34229fcaf1fb56d98c1eaada0e8d
Mathias Bynens <mathias@qiwi.be>
2020-12-10 15:58:55 +0100

    Update index.js

--- cli/src/index.js
+++ cli/src/index.js
@@ -19,11 +19,11 @@ function clamp(v, min, max) {
   return v;
 }
 
-const suffix = ['B', 'KB', 'MB'];
+const suffix = ['B', 'KiB', 'MiB'];
 function prettyPrintSize(size) {
   const base = Math.floor(Math.log2(size) / 10);
   const index = clamp(base, 0, 2);
-  return (size / 2 ** (10 * index)).toFixed(2) + suffix[index];
+  return (size / 2 ** (10 * index)).toFixed(2) + ' ' + suffix[index];
 }
 
 async function decodeFile(file) {
