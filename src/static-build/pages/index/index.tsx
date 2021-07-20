b37cc0784f5f0a95521c1c2fbe818604fe373f5f
Mike DelGaudio <43451174+mikedelgaudio@users.noreply.github.com>
2021-07-19 20:31:58 -0400

    typo fix for secure

--- src/static-build/pages/index/index.tsx
+++ src/static-build/pages/index/index.tsx
@@ -39,7 +39,7 @@ const Index: FunctionalComponent<Props> = () => (
       <meta property="og:title" content="Squoosh" />
       <meta property="og:type" content="website" />
       <meta property="og:image" content={ogImage} />
-      <meta property="og:image:secure" content={ogImage} />
+      <meta property="og:image:secure_url" content={ogImage} />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="500" />
       <meta property="og:image:height" content="500" />
