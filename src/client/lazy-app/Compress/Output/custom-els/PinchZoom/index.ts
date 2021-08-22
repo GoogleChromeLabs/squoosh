470e156b510f686df9ab0b9c3e4a5bb2ebc8ce16
Vishal Chaudhary <vishal38785@gmail.com>
2021-08-23 00:41:24 +0530

    Reduce max scale

--- src/client/lazy-app/Compress/Output/custom-els/PinchZoom/index.ts
+++ src/client/lazy-app/Compress/Output/custom-els/PinchZoom/index.ts
@@ -81,7 +81,7 @@ function createPoint(): SVGPoint {
 }
 
 const MIN_SCALE = 0.01;
-const MAX_SCALE = 1000000;
+const MAX_SCALE = 100000;
 
 export default class PinchZoom extends HTMLElement {
   // The element that we'll transform.
