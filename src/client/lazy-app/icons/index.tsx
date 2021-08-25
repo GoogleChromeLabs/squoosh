1c2b19f2be5312a13a360877f5f1dcba81ceff98
Vishal Chaudhary <vishal38785@gmail.com>
2021-08-26 01:35:35 +0530

    Aliasing SVG icon

--- src/client/lazy-app/icons/index.tsx
+++ src/client/lazy-app/icons/index.tsx
@@ -1,4 +1,5 @@
 import { h } from 'preact';
+import aliasingLogo from 'url:./imgs/aliasing.svg';
 
 const Icon = (props: preact.JSX.HTMLAttributes) => (
   // @ts-ignore - TS bug https://github.com/microsoft/TypeScript/issues/16019
@@ -12,9 +13,7 @@ const Icon = (props: preact.JSX.HTMLAttributes) => (
 );
 
 export const ToggleAliasingIcon = (props: preact.JSX.HTMLAttributes) => (
-  <Icon {...props}>
-    <path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm2 4v-2H3c0 1.1.9 2 2 2zM3 9h2V7H3v2zm12 12h2v-2h-2v2zm4-18H9a2 2 0 0 0-2 2v10c0 1.1.9 2 2 2h10a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2zm0 12H9V5h10v10zm-8 6h2v-2h-2v2zm-4 0h2v-2H7v2z" />
-  </Icon>
+  <img src={aliasingLogo} alt="Aliasing" width="24" height="24" />
 );
 
 export const ToggleBackgroundIcon = (props: preact.JSX.HTMLAttributes) => (
