fdbf59a1438c28c6d6c8b554bbf9e941d742f285
Tim van der Lippe <tvanderlippe@google.com>
2021-10-21 15:52:24 +0100

    Use different way of listing all subdirectories

--- codecs/build-all.sh
+++ codecs/build-all.sh
@@ -1,5 +1,5 @@
 CURRENT_DIRECTORY=$(dirname "$(readlink -f "$0")")
-SUB_DIRECTORIES=$(find "$CURRENT_DIRECTORY" -mindepth 1 -maxdepth 1 -type d)
+SUB_DIRECTORIES=$(ls -d $CURRENT_DIRECTORY/*/)
 
 for codec in $SUB_DIRECTORIES
 do
