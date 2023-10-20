0b330cd55c5d57b8090f8b4026c8578549bf832a
Jake Archibald <jake.archibald@shopify.com>
2023-10-20 08:53:41 +0100

    Align encoder with jxl

--- src/features/decoders/qoi/worker/qoiDecode.ts
+++ src/features/decoders/qoi/worker/qoiDecode.ts
@@ -1,12 +1,11 @@
-import type { QOIModule } from 'codecs/qoi/dec/qoi_dec';
+import qoiDecoder, { QOIModule } from 'codecs/qoi/dec/qoi_dec';
 import { initEmscriptenModule, blobToArrayBuffer } from 'features/worker-utils';
 
 let emscriptenModule: Promise<QOIModule>;
 
 export default async function decode(blob: Blob): Promise<ImageData> {
   if (!emscriptenModule) {
-    const decoder = await import('codecs/qoi/dec/qoi_dec');
-    emscriptenModule = initEmscriptenModule(decoder.default);
+    emscriptenModule = initEmscriptenModule(qoiDecoder);
   }
 
   const [module, data] = await Promise.all([
